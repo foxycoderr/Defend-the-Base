76f845d36e96743cf1e28ee70f05657438c99d14
Bykov Platon <103729245+foxycoderr@users.noreply.github.com>
2022-12-03 23:32:24 +0200

    Smoothened up movement, created new path map.

--- Source/Objects/monster_base.py
+++ Source/Objects/monster_base.py
@@ -2,36 +2,39 @@ import pygame
 from Source.settings import Settings
 import math
 
+
 class MonsterBase:
     def __init__(self, path):
         self.rect = pygame.Rect(0.0, 637.0, 30.0, 30.0)
         self.color = pygame.Color("green")
+        self.image = None
         self.path = path
         self.progress = 0
-        self.destination = [56, 584]
+        self.destination = self.path[1]
         self.degrees = 0
         self.shift_x = 0
         self.shift_y = 0
         self.x = 0.0
         self.y = 637.0
+        self.speed = 1
 
     def move(self):
         deltaX = self.destination[0] - self.rect.x
         deltaY = self.destination[1] - self.rect.y
 
-        print(f"Deltas: {deltaX}, {deltaY}")
+        #print(f"Deltas: {deltaX}, {deltaY}")
         degrees = math.atan2(deltaX, deltaY)/math.pi*180
 
-        print(degrees)
+        #print(degrees)
         if degrees < 0:
             degrees = 360 + degrees
 
         self.degrees = degrees
 
-        self.shift_x = math.cos(math.radians(int(90 - self.degrees)))
-        self.shift_y = math.sin(math.radians(int(90 - self.degrees)))
+        self.shift_x = math.cos(math.radians(int(90 - self.degrees))) * self.speed
+        self.shift_y = math.sin(math.radians(int(90 - self.degrees))) * self.speed
 
-        print([self.shift_x, self.shift_y])
+        #print([self.shift_x, self.shift_y])
         self.x += self.shift_x
         self.y += self.shift_y
 
@@ -39,9 +42,8 @@ class MonsterBase:
         self.rect.y = self.y
 
         if abs(self.destination[0] - self.x) < 5 and abs(self.destination[1] - self.y) < 5:
-            if self.progress == len(self.path) - 1:
-                exit()
-            self.progress += 1
+            if not self.progress == len(self.path) - 1:
+                self.progress += 1
             self.destination = self.path[self.progress]
 
 
--- Source/application.py
+++ Source/application.py
@@ -1,6 +1,7 @@
 import pygame
 from Source.Scenes.game import GameScene
 from Source.settings import Settings
+
 import time
 
 class Application:
@@ -11,12 +12,14 @@ class Application:
 
     def process_frame(self):
 
-        Settings.SCREEN.fill((0, 0, 0))
+        #Settings.SCREEN.fill((0, 0, 0))
         for scene in self.scenes:
             scene.logic()
             scene.draw()
 
         pygame.display.flip()
+
+
         #time.sleep(0.1)
 
     def start(self):
